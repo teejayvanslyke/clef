eec4ec18883c3908eac86c204f695069ec8aa8cf
T.J. VanSlyke <teejayvanslyke@tj-vanslykes-macbook-pro.local>
2009-06-21 12:47:33 -0700

    Made midi interface a mock to speed up tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,5 +5,15 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'clef'
 
 Spec::Runner.configure do |config|
+
+  config.before(:each) do
+    midi = mock(MIDIator::Interface,
+                :control_change => nil,
+                :program_change => nil,
+                :note_on        => nil,
+                :note_off       => nil,
+                :autodetect_driver => nil)
+    MIDIator::Interface.stub!(:new).and_return(midi)
+  end
   
 end
